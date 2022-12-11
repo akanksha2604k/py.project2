81ec092ad28633742fbf74fa639ac8a712c92af7
AKANKSHA KUMARI <120215134+akanksha2604k@users.noreply.github.com>
2022-12-11 18:39:44 +0530

    Add files via upload

---
+++ ak2.py
@@ -0,0 +1,16 @@
+import turtle as t 
+t.penup()
+t.backward(120)
+t.pendown()
+t.speed(10)
+t.hideturtle()
+a = 200
+for i in range(20):
+    t.fd(a)
+    t.left(120)
+    a -= 10
+for i in range(20):
+    t.fd(a)
+    t.left(120)
+    a -= 10
+t.mainloop
